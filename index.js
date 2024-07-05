1b945b42bcffe087ce19b9f477f398417b31801d
Aron <aron.kipkorir@student.moringaschool.com>
2024-07-05 16:41:37 +0300

    adds move dogde right and left

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    
+    const currentLeft = parseInt(dodger.style.left, 10);
+    
+    const moveLeft = 10; 
+    
+    const newLeft = currentLeft - moveLeft;
+    
+    dodger.style.left = `${newLeft}px`;
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    
+    const currentLeft = parseInt(dodger.style.left, 10);
+    
+    const moveRight = 10; 
+
+    const newLeft = currentLeft + moveRight;
+    
+    dodger.style.left = `${newLeft}px`;
+  }
+  
